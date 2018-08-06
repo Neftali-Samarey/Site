89c12bb829660ac8e2b664bb3b414244a765a4ad
Neftali-Samarey <nquintero@araca.com>
2018-08-06 14:28:20 -0400

    Initial Commit

---
+++ site/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+class MainHeader extends Component {
+  render () {
+
+    var headerStyling = {
+          backgroundColor: "#ff6b6b",
+          height: "80vh"
+      };
+    return (
+      /*Scafolding the main header */
+      <div class="" style={headerStyling}>
+        <div id="brand">
+       
+        </div>
+      </div>
+      );
+  }
+}
+
+
+
+export default MainHeader;
